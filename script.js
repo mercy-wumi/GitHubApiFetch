ac7e257d878abac6e4c903752529d590b2c12c19
mercy-wumi <m.adams1909@gmail.com>
2022-01-31 05:28:05 +0100

    a responsive webpage to fetch users from github api

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict'
+//https://api.github.com/users/repos
+
+function findRepos(username) {
+    fetch(`https://api.github.com/users/${username}`)
+        .then(response => response.json())
+        .then(responseJson => displayRepos(responseJson))
+}
+
+function displayRepos(responseJson) {
+    console.log(responseJson);
+    $('#result-container').html('');
+    $('#result-container').append(`
+            <div id='results'>
+            <img src='${responseJson.avatar_url}' alt='git-avatar' />
+            <div id='git-info'>
+                <p>Name: ${responseJson.name}</p>
+                <p>Number of rpublic epos: ${responseJson.public_repos}</p>
+                <a href='${responseJson.html_url}'>${responseJson.html_url}</a>
+            </div>
+            </div>
+  `
+    )
+}
+
+function watchForm() {
+    $('#form').submit(event => {
+        event.preventDefault();
+        const username = event.target.username.value;
+        findRepos(username)
+        event.target.username.value = ''
+    })
+}
+
+$(function () {
+    console.log("My App is loaded! Submission awaits!")
+    watchForm()
+})
